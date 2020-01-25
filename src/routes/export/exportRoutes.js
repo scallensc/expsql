2947a7bf973a0feac90c14c2f8692fda86326f78
Samuel Allen <samuelcallen@me.com>
2020-01-25 22:12:45 +1100

    added links in landing page to retrieve all data from each available table

--- src/routes/export/exportRoutes.js
+++ src/routes/export/exportRoutes.js
@@ -26,6 +26,25 @@ exportRouter.get('/', (req, res, next) => {
     }
     next();
 })
+
+exportRouter.use('/:id', (req, res, next) => {
+    const db = new sqlite3.Database('./src/db/mocks.db', (err) => {
+        if (err) {
+            console.error(`Error connecting to database: ${err}`)
+        }
+    })
+    const tableId = req.params.id;
+    db.all(`SELECT * FROM ${tableId}`, function (err, rows) {
+        if (err) {
+            console.log(err.message);
+        };
+        let output = JSON.stringify(rows, null, 4)
+        console.table(JSON.parse(output))
+        res.send(JSON.parse(output))
+        res.end()
+    });
+});
+
 exportRouter.use((req, res) => {
     res.send('Data exported to "/data/"')
 })
--- src/routes/router.js
+++ src/routes/router.js
@@ -28,8 +28,23 @@ router.get('/', (req, res) => {
                         Visit <a href="/posts">/posts</a> to generate a new post from a random user ID
                     </li>
                     <li>
-                    Visit <a href="/export">/export</a> to export the information in the database to JSON
+                        Visit <a href="/export">/export</a> to export ALL the information in the database to JSON
                     </br> Files will be located in "/data"
+                    </br></br>
+                    <li>
+                        Visit <a href="/export/users">/export/users</a> to retrieve ALL info from users table
+                    </li>
+                    <li>
+                        Visit <a href="/export/posts">/export/posts</a> to retrieve ALL info from posts table
+                    </li>
+                    <li>
+                        Visit <a href="/export/tags">/export/tags</a> to retrieve ALL info from tags table
+                    </li>
+                    <li>
+                        Visit <a href="/export/posts_tags">/export/users</a> to retrieve ALL info from posts_tags table
+                    </li>
+                    <li>
+                        Visit <a href="/export/posts_authors">/export/posts_authors</a> to retrieve ALL info from posts_authors table
                     </li>
             </section>
         </body>
