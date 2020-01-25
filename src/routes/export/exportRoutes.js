8f928a404572876131e05633fef79fb829bc8ffb
Samuel Allen <samuelcallen@me.com>
2020-01-25 15:54:36 +1100

    update

--- src/routes/export/exportRoutes.js
+++ src/routes/export/exportRoutes.js
@@ -1,15 +1,20 @@
 const exportRouter = require('express').Router()
-const exportModel = require('../../models/export')
-
 
 exportRouter.get('/', (req, res, next) => {
-    res.body = exportModel.exportTables()
-    next()
-})
-
-exportRouter.use((req, res) => {
-    exportModel.exportTables(res.body)
-    res.send(res.body)
-})
+    const SqliteJsonExport = require('sqlite-json-export');
+    const sqlite3 = require('sqlite3');
+    const exporter = new SqliteJsonExport('./src/db/mocks.db');
+    const db = new sqlite3.Database('./src/db/mocks.db', (err) => {
+        if (err) {
+            console.error(`Error connecting to database: ${err}`)
+        }
+    exporter.save('users', 'data/users.json', (err, data) => {});
+    exporter.save('posts', 'data/posts.json', (err, data) => {});
+    exporter.save('tags', 'data/tags.json', (err, data) => {});
+    exporter.save('posts_authors', 'data/posts_authors.json', (err, data) => {});
+    exporter.save('posts_tags', 'data/posts_tags.json', (err, data) => {});
+    db.close()
+    })
+});
 
 module.exports = exportRouter
